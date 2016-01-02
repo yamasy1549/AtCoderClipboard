3fe9a6d875237b5d99407f542e980c386f4c70d1
yamasy1549 <sanae@yamasy.info>
2016-01-02 23:18:21 +0900

    Add js files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var i = 0;
+    lits = $(".L0 > .lit");
+    lits.each(function(i){
+        if(i%2 == 0) {
+            var value = lits[i].innerHTML;
+            $(this).after('<button data-clipboard-text="' + value + '" class="copyButton">Copy!</button>');
+        }
+    });
+
+    var cp = new Clipboard('.copyButton');
+});
