18699944eeb2a5f86db36319115a7764a7411efc
yamasy1549 <sanae@yamasy.info>
2016-01-04 12:51:32 +0900

    複数行の入力に対応

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
     var i = 0;
-    lits = $(".L0 > .lit");
-    lits.each(function(i){
-        if(i%2 == 0) {
-            var value = lits[i].innerHTML;
-            $(this).after('<button data-clipboard-text="' + value + '" class="copyButton">Copy</button>');
+    linenums = $(".prettyprint:even > .linenums");
+    linenums.each(function(i){
+        var linenum = linenums[i];
+        var str = "";
+        for(var j = 0; j < linenum.children.length; j++) {
+            for(var k = 0; k < linenum.children[j].children.length; k++) {
+                str += linenum.children[j].children[k].innerHTML;
+            }
+            str += "\n"
         }
+        var h3 = linenum.closest(".part").children[0]
+        $('<button data-clipboard-text="' + str + '" class="copyButton">Copy</button>').appendTo(linenum.closest(".part").children[0]);
     });
 
     var cp = new Clipboard('.copyButton');
